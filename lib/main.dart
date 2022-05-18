3c0056f3bd697711fa067d295886f1736e9f0eb2
lumeijin <851039024@qq.com>
2022-05-18 12:46:53 +0800

    intruduce an GridViewLayout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'res/listData.dart';
 
 void main() {
   runApp(const MyApp());
@@ -62,7 +63,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: BodyContent(),
+      body: GridViewLayout(),
       // This trailing comma makes auto-formatting nicer for build methods.
       drawer: Drawer(
         child: ListView(
@@ -115,10 +116,10 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-class BodyContent extends StatelessWidget {
+class ListViewLayout extends StatelessWidget {
   var list = [];
 
-  BodyContent({Key? key}) : super(key: key) {
+  ListViewLayout({Key? key}) : super(key: key) {
     for (var i = 0; i < 18; i++) {
       list.add("元神${i + 1}");
     }
@@ -129,21 +130,57 @@ class BodyContent extends StatelessWidget {
     // TODO: implement build
     return Center(
       child: ListView.builder(
-          itemCount: list.length, itemBuilder: (context, index) {
-          return ListTile(
-            leading: CircleAvatar(
-            //头像半径
-            radius: 60,
-            //头像图片 -> NetworkImage网络图片，AssetImage项目资源包图片, FileImage本地存储图片
-            backgroundImage: AssetImage(
-              'images/ys${index + 1}.png',
-            ),
-          ),
-            title: Text(
-              "元神${index + 1}", style: const TextStyle(fontSize: 28),),
-            subtitle: Text("详细说明${index + 1}"),
-          );}
-      ),
+          itemCount: list.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: CircleAvatar(
+                //头像半径
+                radius: 60,
+                //头像图片 -> NetworkImage网络图片，AssetImage项目资源包图片, FileImage本地存储图片
+                backgroundImage: AssetImage(
+                  'images/ys${index + 1}.png',
+                ),
+              ),
+              title: Text(
+                "元神${index + 1}",
+                style: const TextStyle(fontSize: 28),
+              ),
+              subtitle: Text("详细说明${index + 1}"),
+            );
+          }),
     );
+  }
+}
+
+class GridViewLayout extends StatelessWidget {
+  List<Widget> _getDataList() {
+    var tempList = listData.map((value) {
+      return Container(
+          child: Column(
+            children: <Widget>[
+              Image.asset(value["imageUrl"]),
+              const SizedBox(height: 12),
+              Text(value["title"],
+                  textAlign: TextAlign.center, style: const TextStyle(fontSize: 20.0)),
+            ],
+          ),
+          decoration: BoxDecoration(
+              border: Border.all(
+                  color: const Color.fromRGBO(230, 230, 230, 0.9), width: 1.0)));
+    });
+    return tempList.toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return GridView.count(
+        padding: EdgeInsets.all(20),
+    crossAxisCount: 2,
+    crossAxisSpacing: 20,
+    mainAxisSpacing: 40, // childAspectRatio:0.7,
+    children: _getDataList(),
+    );
+    // throw UnimplementedError();
   }
 }
---
+++ lib/res/listData.dart
@@ -0,0 +1,37 @@
+List listData = [
+  {
+    "title": 'Candy Shop',
+    "author": 'Mohamed Chahin',
+    "imageUrl": 'images/ys1.png',
+  },
+  {
+    "title": 'Childhood',
+    "author": 'Google',
+    "imageUrl": 'images/ys2.png',
+  },
+  {
+    "title": 'Alibaba Shop',
+    "author": 'Alibaba',
+    "imageUrl": 'images/ys3.png',
+  },
+  {
+    "title": 'Candy Shop',
+    "author": 'Mohamed Chahin',
+    "imageUrl": 'images/ys4.png',
+  },
+  {
+    "title": 'Tornado',
+    "author": 'Mohamed Chahin',
+    "imageUrl": 'images/ys5.png',
+  },
+  {
+    "title": 'Undo',
+    "author": 'Mohamed Chahin',
+    "imageUrl": 'images/ys6.png',
+  },
+  {
+    "title": 'white-dragon',
+    "author": 'Mohamed Chahin',
+    "imageUrl": 'images/ys7.png',
+  }
+];
