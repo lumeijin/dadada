411d7695148e00fd01bdb696ce6e0f2e7b14c0ee
lumeijin <851039024@qq.com>
2022-05-19 17:09:22 +0800

    create a file 'gridview_layout.dart'

---
+++ lib/layout/gridview_layout.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import '../res/listdata.dart';
+
+class GridViewLayout extends StatelessWidget {
+  const GridViewLayout({Key? key}) : super(key: key);
+
+  List<Widget> _getDataList() {
+    var tempList = listData.map((value) {
+      return Container(
+          child: Column(
+            children: <Widget>[
+              Image.asset(value["imageUrl"]),
+              const SizedBox(height: 12),
+              Text(value["title"],
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 20.0)),
+            ],
+          ),
+          decoration: BoxDecoration(
+              border: Border.all(
+                  color: const Color.fromRGBO(230, 230, 230, 0.9),
+                  width: 1.0)));
+    });
+    return tempList.toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return GridView.count(
+      padding: EdgeInsets.all(20),
+      crossAxisCount: 2,
+      crossAxisSpacing: 20,
+      mainAxisSpacing: 40,
+      // childAspectRatio:0.7,
+      children: _getDataList(),
+    );
+    // throw UnimplementedError();
+  }
+}
--- lib/pages/tabs/homepage.dart
+++ lib/pages/tabs/homepage.dart
@@ -39,38 +39,3 @@ class HomePage extends StatelessWidget {
 }
 
 
-class GridViewLayout extends StatelessWidget {
-  List<Widget> _getDataList() {
-    var tempList = listData.map((value) {
-      return Container(
-          child: Column(
-            children: <Widget>[
-              Image.asset(value["imageUrl"]),
-              const SizedBox(height: 12),
-              Text(value["title"],
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(fontSize: 20.0)),
-            ],
-          ),
-          decoration: BoxDecoration(
-              border: Border.all(
-                  color: const Color.fromRGBO(230, 230, 230, 0.9),
-                  width: 1.0)));
-    });
-    return tempList.toList();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return GridView.count(
-      padding: EdgeInsets.all(20),
-      crossAxisCount: 2,
-      crossAxisSpacing: 20,
-      mainAxisSpacing: 40,
-      // childAspectRatio:0.7,
-      children: _getDataList(),
-    );
-    // throw UnimplementedError();
-  }
-}
